9eb272b7a0ebb5fc77dd58a8de7fdc0a8015ac68
Maxime Lardenois <maxime.lardenois@gmail.com>
2023-03-30 11:30:28 +0200

    chore:Added linting and formatting tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import './App.css';
 
 function App() {
+  const tot = '';
   return (
     <div className="App">
       <h1>City Weather Forecast</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
