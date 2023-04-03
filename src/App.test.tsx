a9723ae1b7e214cf5d5331347e2b7ef7c8bf268b
Maxime Lardenois <maxime.lardenois@gmail.com>
2023-04-03 16:48:23 +0200

    test:Add test on App to check interactivity

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 import App from './App';
 
-import { render, screen } from '../test-utils';
+import { render, screen, userEvent } from '../test-utils';
+import { cities } from './data/cities';
 
 describe('Home test', () => {
   it('the title is visible', () => {
@@ -12,6 +13,17 @@ describe('Home test', () => {
     const helpText = screen.getByText(
       /Please click on a city to get forecast/i,
     );
+    expect(helpText).toBeInTheDocument();
+  });
+
+  it('should display forecast on city selection', async () => {
+    const user = userEvent.setup();
+    render(<App />);
+    const firstMarker = screen.getByRole('button', {
+      description: cities[0].name,
+    });
+    await user.click(firstMarker);
+    const helpText = screen.getByText(`3-day forecast for ${cities[0].name}`);
     expect(helpText).toBeInTheDocument();
   });
 });
