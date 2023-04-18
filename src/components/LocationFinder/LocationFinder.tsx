1a7d1a6e0b3e5e7310707047a1b55ebc7716ec0b
Maxime Lardenois <maxime.lardenois@gmail.com>
2023-04-18 10:56:11 +0200

    feat:Add spinner on location search

--- src/components/LocationFinder/LocationFinder.tsx
+++ src/components/LocationFinder/LocationFinder.tsx
@@ -1,11 +1,13 @@
 import { ChangeEventHandler, useContext, useRef, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+import { CgSpinnerTwoAlt } from 'react-icons/cg';
 import { FavoriteCitiesContext } from '@/contexts/FavoriteCities.context';
 import { SelectedCityContext } from '@/contexts/SelectedCity.context';
 import { geocode } from '@/utils/owm-client/owm-client';
 
 function LocationFinder() {
   const [searchString, setSearchString] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const { addCityToFavorites } = useContext(FavoriteCitiesContext);
   const { setCurrentCity } = useContext(SelectedCityContext);
   const searchInput = useRef<HTMLInputElement>(null);
@@ -18,19 +20,22 @@ function LocationFinder() {
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
     if (searchString === '') return;
+    setIsLoading(true);
     const cities = await geocode(searchString);
     if (cities && cities.length > 0) {
-      addCityToFavorites(cities[0]);
-      setCurrentCity(cities[0]);
+      const favCity = addCityToFavorites(cities[0]);
+      setCurrentCity(favCity);
       setSearchString('');
       searchInput.current?.blur();
     }
+    setIsLoading(false);
   };
 
   return (
     <div className="w-full rounded bg-slate-800 bg-opacity-95 p-3 text-left shadow-xl hover:bg-opacity-100 md:max-w-xl md:p-4">
       <form onSubmit={handleSubmit} className="flex justify-between">
         <input
+          disabled={isLoading}
           type="text"
           value={searchString}
           onChange={handleTextChange}
@@ -38,11 +43,22 @@ function LocationFinder() {
           ref={searchInput}
           className="w-full bg-transparent text-xl text-white caret-white focus:outline-none md:w-96 md:text-2xl"
         />
-        <button type="submit" value="Search">
-          <FaSearch
-            className="text-2xl text-white"
-            title="Click to search location"
-          />
+        <button
+          type="submit"
+          value="Search"
+          disabled={isLoading || !searchString}
+        >
+          {!isLoading ? (
+            <FaSearch
+              className="text-2xl text-white"
+              title="Click to search location"
+            />
+          ) : (
+            <CgSpinnerTwoAlt
+              className="animate-spin text-2xl text-white"
+              title="Click to search location"
+            />
+          )}
         </button>
       </form>
     </div>
